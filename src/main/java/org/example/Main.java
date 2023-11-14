9565c551430130d68ce9421e769c3136741fce00
Amonizz <127430471+Monizza-Andrea@users.noreply.github.com>
2023-11-14 18:40:28 +0100

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,23 +1,9 @@
 package org.example;
 
-import carrello.Carrello;
-import user.PrivatoBuilder;
-import user.Utente;
-
 public class Main {
 
     public static void main(String[] args) {
 
-        PrivatoBuilder builder = new PrivatoBuilder();
-        builder.setName("Luca");
-        builder.setSurname("Savini");
-        builder.setEmail("SaviniLucaredeRomaplusultra@gmail.ermejo");
-        builder.setPassword("iosoioevoinonsieteuncazzo");
-        builder.setSpesa(new Carrello());
-
-        Utente utenteLuca2 = builder.build();
-
-
-        System.out.println(utenteLuca2);
+       System.out.println("Hello word!");
     }
 }
